c0851e7c25f1a7c4167ebdb14cb60a77779aa907
Dimitris Michailidis <jimichailidis@gmail.com>
2019-02-14 14:26:37 +0100

    Added better email comparison on mapper

--- lab2/enron-map.py
+++ lab2/enron-map.py
@@ -3,18 +3,25 @@
 from datetime import datetime
 
 import sys
+
+def get_email_domain(email):
+	return email.split('@')[1]
+
 for line in sys.stdin:
 	data = line.split('\t')
 	# Exclude emails not between the given date
 	date = datetime.strptime(data[0], '%Y-%m-%dT%H:%M:%S%fZ')
 	keep_date = datetime(2001, 11, 5).date() <= date.date() <= datetime(2001, 11, 8).date()
 
-	# Exclude emails not sent outside of enron
-	keep_sndr = '@enron' in data[1]
-	keep_rcpt = '@enron' not in data[2]
+	# Get the domain of the email addresses of the sender and the recipient
+	sndr = get_email_domain(data[1])
+	rcpt = get_email_domain(data[2])
+
+	# Exclude emails from enron not sent outside of enron
+	keep_sndr = 'enron' in sndr
+	keep_rcpt = 'enron' not in rcpt
 
 	if keep_date and keep_sndr and keep_rcpt:
-		# Emit the sender to the reduce.
+		# Emit the sender to the reducer.
 		print data[1].strip()
-		# for item in data:
-		# 	print item.strip()+ "\t"
+
